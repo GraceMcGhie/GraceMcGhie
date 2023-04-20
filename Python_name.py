9cca51b83775058c90d2a004ed011b44cac11bf4
GraceMcPoco <86392565+GraceMcGhie@users.noreply.github.com>
2023-04-20 18:11:14 +0800

    Update Python_name.py

--- Python_name.py
+++ Python_name.py
@@ -1,16 +1,19 @@
 print ("hello there hooman")
 repeat = True
 while repeat == True:
-    name = input("What is your name?")
+    name = input("What is your name? ")
     if name == 'Grace':
         print ("I like you")
     else: print ('I dislike you')
-    lies = input ('Are you sure this is your name?')
+    lies = input ('Are you sure this is your name? ')
     if lies == 'yes':
         repeat = False
     elif lies == 'no':
         repeat = True
     else:
         lies = input ("I wasn't listening so guess what you have to say your name again :)")
-        repeat = True\
+        repeat = True
+print ('My name is computer. I eat hoomans')
+food = input ('What do you eat? ')
+print ("Thats delicous, something I've always wanted to try. Some good old " + food)
 # well all in a days work
